3e29c12d825cc89376f92b83b1e78ce7167da606
Gabi Ichim <gabiichim230@yahoo.com>
2023-05-26 00:09:35 +0300

    Added .gitkeep for teaching materials download folder, added singleton for repos.

--- EducationalPlatform/EducationalPlatform/Bootstrapper.cs
+++ EducationalPlatform/EducationalPlatform/Bootstrapper.cs
@@ -45,22 +45,21 @@ namespace EducationalPlatform
             builder.RegisterAssemblyTypes(Assembly.Load("EducationalPlatform"))
                 .Where(a => a.Namespace.Contains("ViewModels.StudentViewModels")).AsSelf();
             builder.RegisterType<AuthenticationViewModel>().AsSelf();
-            builder.RegisterType<ViewModelBase>().AsSelf();
 
             builder.RegisterAssemblyTypes(Assembly.Load("EducationalPlatform.DataAccess"))
                 .Where(a => a.Namespace.Contains("Models")).AsSelf();
 
             builder.RegisterType<EducationalPlatformDbContext>().AsSelf().SingleInstance();
 
-            builder.RegisterType<AbsenceRepository>().As<IRepository<Absence>>();
-            builder.RegisterType<ClassroomRepository>().As<IRepository<Classroom>>();
-            builder.RegisterType<GradeRepository>().As<IRepository<Grade>>();
-            builder.RegisterType<PersonRepository>().As<IRepository<Person>>();
-            builder.RegisterType<SpecializationRepository>().As<IRepository<Specialization>>();
-            builder.RegisterType<StudentRepository>().As<IRepository<Student>>();
-            builder.RegisterType<SubjectRepository>().As<IRepository<Subject>>();
-            builder.RegisterType<TeacherRepository>().As<IRepository<Teacher>>();
-            builder.RegisterType<TeachingMaterialRepository>().As<IRepository<TeachingMaterial>>();
+            builder.RegisterType<AbsenceRepository>().As<IRepository<Absence>>().SingleInstance();
+            builder.RegisterType<ClassroomRepository>().As<IRepository<Classroom>>().SingleInstance();
+            builder.RegisterType<GradeRepository>().As<IRepository<Grade>>().SingleInstance();
+            builder.RegisterType<PersonRepository>().As<IRepository<Person>>().SingleInstance();
+            builder.RegisterType<SpecializationRepository>().As<IRepository<Specialization>>().SingleInstance();
+            builder.RegisterType<StudentRepository>().As<IRepository<Student>>().SingleInstance();
+            builder.RegisterType<SubjectRepository>().As<IRepository<Subject>>().SingleInstance();
+            builder.RegisterType<TeacherRepository>().As<IRepository<Teacher>>().SingleInstance();
+            builder.RegisterType<TeachingMaterialRepository>().As<IRepository<TeachingMaterial>>().SingleInstance();
 
             return builder.Build(); 
         }
